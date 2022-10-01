06e05f0a0c8e8e63858cc69f27941b44e1476f87
Gideon Boateng <93168616+KwakuBoat@users.noreply.github.com>
2022-10-01 06:47:15 -0400

    New  file # Adding add_card activity # Adding icon_save # Adding icon_cancel

--- app/src/main/java/com/example/flashcard_fall2022/MainActivity.kt
+++ app/src/main/java/com/example/flashcard_fall2022/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.flashcard_fall2022
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +17,9 @@ class MainActivity : AppCompatActivity() {
 
         val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
         val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        val addQuestionButton = findViewById<ImageView>(R.id.add_question_button)
+
+
         flashcardQuestion.setOnClickListener{
             flashcardAnswer.visibility = View.VISIBLE
             flashcardQuestion.visibility = View.INVISIBLE
@@ -27,6 +34,28 @@ class MainActivity : AppCompatActivity() {
             flashcardAnswer.visibility = View.INVISIBLE
             flashcardQuestion.visibility= View.VISIBLE
         }
+
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result ->
+
+            val data : Intent? = result.data
+
+            if (data != null){
+                val questionString = data.getStringExtra("QUESTION_KEY")
+                val answerString = data.getStringExtra("ANSWER_KEY")
+
+                flashcardQuestion.text = questionString
+                flashcardAnswer.text = answerString
+            }else{
+
+            }
+        }
+        addQuestionButton.setOnClickListener{
+            val intent = Intent(this, AddCardActivity::class.java)
+            resultLauncher.launch(intent)
+        }
+
+
 
 
 
